2d81351fa650e96b2b6e8076c02d0d3f74ea8ac1
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-08-12 20:20:56 +0100

    expired and expiring soon;

--- src/models.py
+++ src/models.py
@@ -11,6 +11,20 @@ class Product(db.Model):
     date_added = db.Column(db.db.String(64))
     brand = db.Column(db.String(64))
     image_url = db.Column(db.String(400))
+    barcode = db.Column(db.String(400))
 
     def __repr__(self):
         return f'<Product {self.id}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'categories': self.categories,
+            'expiry_date': self.expiry_date,
+            'date_added': self.date_added,
+            'brand': self.brand,
+            'image_url': self.image_url,
+            'barcode': self.barcode
+        }
--- src/routes/products.py
+++ src/routes/products.py
@@ -1,14 +1,53 @@
 from flask import Blueprint, jsonify
 from src.models import Product
 from flask_jwt_extended import create_access_token, jwt_required
+from datetime import datetime, timedelta
 
 products = Blueprint('products', __name__)
 
 @products.route('/products/<id>', methods=['GET'])
-@jwt_required(id)
-def get_products():
-    products = Product.query.filter_by(user_id=id).first()
-    return jsonify([product.to_dict() for product in products])
+@jwt_required()
+def get_products(id):
+    print(id)
+    products = Product.query.filter_by(user_id=int(id)).all()
+    newlist=[]
+    for product in products:
+        newlist.append(product.to_dict())
+    return jsonify(newlist)
+
+
+@products.route('/expiring-products/<id>', methods=['GET'])
+@jwt_required()
+def get_expiring_products(id):
+    today = datetime.now().date()
+    three_days_from_now = today + timedelta(days=3)
+
+    # Convert all product expiry_dates to datetime objects and compare
+    expiring_products = []
+    products = Product.query.filter_by(user_id=int(id)).all()
+    for product in products:
+        product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y').date()
+
+        if today < product_expiry_date <= three_days_from_now:
+            expiring_products.append(product.to_dict())
+    return jsonify(expiring_products)
+
+@products.route('/expired-products/<id>', methods=['GET'])
+@jwt_required()
+def get_recently_expired_products(id):
+    print(id)
+    ten_days_ago = datetime.now() - timedelta(days=10)
+
+    # Convert all product expiry_dates to datetime objects and compare
+    expired_products = []
+    products = Product.query.filter_by(user_id=int(id)).all()
+    for product in products:
+        product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y')
+        if ten_days_ago <= product_expiry_date < datetime.now():
+            expired_products.append(product.to_dict())
+
+    return jsonify(expired_products)
+
 
 @products.route('/products', methods=['POST'])
 @jwt_required()
