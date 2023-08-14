2f241df0b338421ea722cb95792b803613fbaa3a
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-08-14 17:43:34 +0100

    fix

--- src/models.py
+++ src/models.py
@@ -6,13 +6,15 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     name = db.Column(db.String(64))
-    categories = db.Column(JSONB)
+    categories =  db.Column(db.db.String(204))
     expiry_date = db.Column(db.db.String(64))
     date_added = db.Column(db.db.String(64))
     brand = db.Column(db.String(64))
     image_url = db.Column(db.String(400))
     barcode = db.Column(db.String(400))
     status = db.Column(db.String(400))
+    price = db.Column(db.String(400))
+    waste_level = db.Column(db.String(400))
 
     def to_dict(self):
         return {
@@ -25,5 +27,8 @@ class Product(db.Model):
             'brand': self.brand,
             'image_url': self.image_url,
             'barcode': self.barcode,
-            'status': self.status
+            'status': self.status,
+            'price': self.price,
+            'waste_level': self.waste_level
+
         }
--- src/routes/products.py
+++ src/routes/products.py
@@ -5,15 +5,49 @@ from datetime import datetime, timedelta
 from src import db
 
 products = Blueprint('products', __name__)
+'''
+Status for products:
+
+pantry/fridge
+waste
+consumed
+shopping
+'''
+
+
+@products.route('/products-shopping/<id>', methods=['GET'])
+@jwt_required()
+def get_products_shopping(id):
+    print(id)
+    ten_days_ago = datetime.now() - timedelta(days=10)
+
+    products = Product.query.filter_by(user_id=int(id)).all()
+    newlist=[]
+    for product in products:
+        if product.status == 'shopping':
+            newlist.append(product.to_dict())
+    return jsonify(newlist)
+
+
 
 @products.route('/products/<id>', methods=['GET'])
 @jwt_required()
 def get_products(id):
     print(id)
+    ten_days_ago = datetime.now() - timedelta(days=10)
+
     products = Product.query.filter_by(user_id=int(id)).all()
     newlist=[]
     for product in products:
-        newlist.append(product.to_dict())
+        if product.status != 'shopping'  and product.status != 'waste':
+            if product.expiry_date != '' and product.expiry_date != None:
+                product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y')
+                if ten_days_ago <= product_expiry_date < datetime.now():
+                    print('dont add expired')
+                else:
+                    newlist.append(product.to_dict())
+            else:
+                newlist.append(product.to_dict())
     return jsonify(newlist)
 
 
@@ -27,11 +61,118 @@ def get_expiring_products(id):
     expiring_products = []
     products = Product.query.filter_by(user_id=int(id)).all()
     for product in products:
-        product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y').date()
+        if product.status != 'shopping'  and product.status != 'waste':
 
-        if today < product_expiry_date <= three_days_from_now:
-            expiring_products.append(product.to_dict())
+            if product.expiry_date != '' and product.expiry_date != None:
+                product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y').date()
+
+                if today < product_expiry_date <= three_days_from_now:
+                    expiring_products.append(product.to_dict())
+
     return jsonify(expiring_products)
+
+
+@products.route('/remove-product/<int:product_id>', methods=['GET'])
+def remove_product(product_id):
+    product = Product.query.get(product_id)
+    
+    if product is None:
+        return jsonify({"error": "Product not found"}), 404
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({"message": f"Product {product.id} deleted"}), 200
+
+@products.route('/product/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+
+    if product is None:
+        return jsonify({"error": "Product not found"}), 404
+
+    # Getting the request data
+    data = request.json
+
+    # Update fields based on the provided data
+    if 'name' in data:
+        product.name = data['name']
+    if 'user_id' in data:
+        product.user_id = data['user_id']
+    if 'categories' in data:
+        product.categories = data['categories']
+    if 'expiry_date' in data:
+        product.expiry_date = data['expiry_date']
+    if 'brand' in data:
+        product.brand = data['brand']
+    if 'image_url' in data:
+        product.image_url = data['image_url']
+    if 'barcode' in data:
+        product.barcode = data['barcode']
+    if 'price' in data:
+        product.price = data['price']
+    if 'status' in data:
+        product.status = data['status']
+    if 'waste_level' in data:
+        product.status = data['waste_level']
+
+    db.session.commit()
+
+    return jsonify({"message": f"Product {product.id} updated successfully"}), 200
+
+@products.route('/copy_product-fridge/<int:product_id>', methods=['GET'])
+def copy_product_fridge(product_id):
+    product = Product.query.get(product_id)
+    
+    if product is None:
+        return jsonify({"error": "Product not found"}), 404
+
+    # Create a new product with the same data but status set to 'shopping'
+    new_product = Product(
+        name=product.name,
+        user_id=product.user_id,
+        categories=product.categories,
+        expiry_date='',
+        brand=product.brand,
+        image_url=product.image_url,
+        barcode=product.barcode,
+        price=product.price,
+        status='pantry/fridge'
+    )
+
+    db.session.add(new_product)
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({"message": f"Product {new_product.id} created with status shopping"}), 200
+
+
+
+@products.route('/copy_product-shopping/<int:product_id>', methods=['GET'])
+def copy_product_shopping(product_id):
+    product = Product.query.get(product_id)
+    
+    if product is None:
+        return jsonify({"error": "Product not found"}), 404
+
+    # Create a new product with the same data but status set to 'shopping'
+    new_product = Product(
+        name=product.name,
+        user_id=product.user_id,
+        categories=product.categories,
+        expiry_date='',
+        brand=product.brand,
+        image_url=product.image_url,
+        barcode=product.barcode,
+        price=product.price,
+        status='shopping'
+    )
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return jsonify({"message": f"Product {new_product.id} created with status shopping"}), 200
+
 
 @products.route('/expired-products/<id>', methods=['GET'])
 @jwt_required()
@@ -43,9 +184,11 @@ def get_recently_expired_products(id):
     expired_products = []
     products = Product.query.filter_by(user_id=int(id)).all()
     for product in products:
-        product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y')
-        if ten_days_ago <= product_expiry_date < datetime.now():
-            expired_products.append(product.to_dict())
+        if product.status != 'shopping'  and product.status != 'waste':
+            if product.expiry_date != '' and product.expiry_date != None:
+                product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y')
+                if ten_days_ago <= product_expiry_date < datetime.now():
+                    expired_products.append(product.to_dict())
 
     return jsonify(expired_products)
 
@@ -62,20 +205,22 @@ def recipe_products(id):
     products = Product.query.filter_by(user_id=int(id)).all()
 
     for product in products:
-        product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y').date()
+        if product.expiry_date != '' and product.expiry_date != None:
 
-        if today < product_expiry_date <= three_days_from_now:
+            product_expiry_date = datetime.strptime(product.expiry_date, '%d/%m/%Y').date()
 
-            if expiring_products_str:
-                expiring_products_str += ", "
-            expiring_products_str += product.to_dict()['name']
+            if today < product_expiry_date <= three_days_from_now:
 
-        elif product_expiry_date < today:
-            print('ignore')
-        else:
-            if other_products_str:
-                other_products_str += ", "
-            other_products_str += product.to_dict()['name']
+                if expiring_products_str:
+                    expiring_products_str += ", "
+                expiring_products_str += product.to_dict()['name']
+
+            elif product_expiry_date < today:
+                print('ignore')
+            else:
+                if other_products_str:
+                    other_products_str += ", "
+                other_products_str += product.to_dict()['name']
 
     response = {
         "expiring_products": expiring_products_str,
